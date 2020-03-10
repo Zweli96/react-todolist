fb6c38ca0779e1b8c1682083de8b47977417f6b5
Zwelithini Golowa <itszweli@hotmail.com>
2020-03-10 22:42:33 +0200

    Alteration to the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,10 +4,7 @@ export default function About() {
   return (
     <React.Fragment>
       <h1>About</h1>
-      <p>
-        This is the todo list app version 1.0.0 part of the react crash course
-        app
-      </p>
+      <p>This is the todo list app version 1.0.0 Developed by Linda Labs</p>
     </React.Fragment>
   );
 }
